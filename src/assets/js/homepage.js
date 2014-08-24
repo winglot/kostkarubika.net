c8e3b8ac0a03995b574f6b5faeb5c6eafa6b090c
Wojciech Inglot <wojciech@inglot.net.pl>
2014-08-24 19:55:59 +0200

    Redesigned layout

---
+++ src/assets/js/homepage.js
@@ -0,0 +1,25 @@
+$(function() {
+    $(".cube-wall").each(function() {
+        var wall = new freewall(this);
+        wall.reset({
+            selector: '.brick',
+            animate: true,
+            cellW: 160,
+            cellH: 160,
+            onResize: function() {
+                wall.fitWidth();
+            }
+        });
+        
+        wall.fitWidth();
+    });
+
+    // handle the mouseenter functionality
+    $(".brick").mouseenter(function(){
+        $(this).addClass("hover");
+    })
+    // handle the mouseleave functionality
+    .mouseleave(function(){
+        $(this).removeClass("hover");
+    });
+});
