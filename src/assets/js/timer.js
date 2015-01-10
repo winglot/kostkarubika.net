1174796c475f96f36a18a1a423698aa19ffbad80
Wojciech Inglot <wojciech@inglot.net.pl>
2015-01-11 00:14:10 +0100

    Refactor timer. Add scrambler

--- src/assets/js/timer.js
+++ src/assets/js/timer.js
@@ -1,82 +1,50 @@
 $(function() {
-
-    function Cube() {
-        // Define the six faces of the cube
-        this.faces = "DLBURF";
-
-        // This will contain a history of all the states to make sure we don't repeat a state
-        this.states = [];
-
-        // Which stickers are part of the same layer and should move along with the face
-        this.edges = {
-            D: [46, 45, 44, 38, 37, 36, 22, 21, 20, 14, 13, 12],
-            L: [24, 31, 30, 40, 47, 46, 0, 7, 6, 20, 19, 18],
-            B: [26, 25, 24, 8, 15, 14, 6, 5, 4, 36, 35, 34],
-            U: [18, 17, 16, 34, 33, 32, 42, 41, 40, 10, 9, 8],
-            R: [28, 27, 26, 16, 23, 22, 4, 3, 2, 44, 43, 42],
-            F: [30, 29, 28, 32, 39, 38, 2, 1, 0, 12, 11, 10]
-        };
-
-        // Sets the cube to the solved state
-        this.reset = function () {
-            this.states = ["yyyyyyyyoooooooobbbbbbbbwwwwwwwwrrrrrrrrgggggggg"];
-        };
-
-        // Twist the cube according to a move in WCA notation
-        this.twist = function (state, move) {
-            var i, k, prevState, face = move.charAt(0), faceIndex = this.faces.indexOf(move.charAt(0)),
-                turns = move.length > 1 ? (move.charAt(1) === "2" ? 2 : 3) : 1;
-            state = state.split("");
-            for (i = 0; i < turns; i++) {
-                prevState = state.slice(0);
-                // Rotate the stickers on the face itself
-                for (k = 0; k < 8; k++) { state[(faceIndex * 8) + k] = prevState[(faceIndex * 8) + ((k + 6) % 8)]; }
-                // Rotate the adjacent stickers that are part of the same layer
-                for (k = 0; k < 12; k++) { state[this.edges[face][k]] = prevState[this.edges[face][(k + 9) % 12]]; }
-            }
-            return state.join("");
-        };
+    function CubeScrambler(opts) {
+        // Defined default options
+        this.opts = $.extend({
+            faces: "DLBURF",
+            modifiers: ["", "'", "2"],
+            total: 25 
+        }, opts || {});
 
         // Scramble the cube
         this.scramble = function () {
-            var count = 0, total = 25, state, prevState = this.states[this.states.length - 1],
-                move, moves = [], modifiers = ["", "'", "2"];
-            while (count < total) {
+            var count = 0, move, moves = [];
+            while (count < this.opts.total) {
                 // Generate a random move
-                move = this.faces[Math.floor(Math.random() * 6)] + modifiers[Math.floor(Math.random() * 3)];
+                move = this.opts.faces[Math.floor(Math.random() * 6)] + this.opts.modifiers[Math.floor(Math.random() * 3)];
+
                 // Don't move the same face twice in a row
                 if (count > 0 && move.charAt(0) === moves[count - 1].charAt(0)) { continue; }
+
                 // Avoid move sequences like "R L R", which is the same as "R2 L"
                 if (count > 1 && move.charAt(0) === moves[count - 2].charAt(0) &&
-                        moves[count - 1].charAt(0) === this.faces.charAt((this.faces.indexOf(move.charAt(0)) + 3) % 6)) {
+                        moves[count - 1].charAt(0) === this.opts.faces.charAt((this.opts.faces.indexOf(move.charAt(0)) + 3) % 6)) {
                     continue;
                 }
-                state = this.twist(prevState, move);
-                if (this.states.indexOf(state) === -1) {
-                    // If this state hasn't yet been encountered, save it and move on
-                    moves[count] = move;
-                    this.states[count] = state;
-                    count++;
-                    prevState = state;
-                }
+
+                moves[count] = move;
+                count++;
             }
+
             return moves;
         };
     };
 
-    // Solve time formatter
-    function SolveTime(time) {
-        this.solveTime = time;
+    // Solve time model
+    function SolveTime(opts) {
+        this.opts = $.extend({
+            time: "undefined",
+            pop: false, 
+            plustwo: false
+        }, opts || {});
 
-        this.getFormatted = function() {
-            str = 
-                this._roundTo(this.solveTime.getMinutes(), 2) +
-                ':' +
-                this._roundTo(this.solveTime.getSeconds(), 2) +
-                '.' +
-                this._roundTo(this.solveTime.getMilliseconds(), 3);
+        this.getTime = function() {
+            if (this.opts.plustwo) {
+                return this.opts.time.getTime() + 2000;
+            }
 
-            return str;
+            return this.opts.time.getTime();
         }
 
         this._roundTo = function(t, r) {
@@ -93,38 +61,73 @@ $(function() {
 
             return out;
         }
+
+        this.toString = function() {
+            if (this.opts.pop) {
+                return "POP";
+            }
+
+            str = 
+                this._roundTo(this.opts.time.getMinutes(), 2) +
+                ':' +
+                this._roundTo(this.opts.time.getSeconds(), 2) +
+                '.' +
+                this._roundTo(this.opts.time.getMilliseconds(), 3);
+
+            if (this.opts.plustwo) {
+                str = str + " (+2)";
+            }
+
+            return str;
+        }
     }
 
-    // Holds information about solve time history
-    function TimeHistory(el) {
-        this.el = el;
-        this.entries = [];
+    // Holds information about solve session
+    function SolveSession(opts) {
+        this.opts = $.extend({
+            entries: []
+        }, opts || {});
 
-        this.addEntry = function(time, scramble, pop) {
-            this.entries.push({
+        this.addEntry = function(time, scramble) {
+            this.opts.entries.push({
                 "time": time,
-                "scramble": scramble,
-                "pop": pop
+                "scramble": scramble
             });
 
-            this._renderEntry(this.entries[this.entries.length - 1], this.entries.length);
+            // this._renderEntry(this.opts.entries[this.opts.entries.length - 1], this.opts.entries.length);
         }
 
+        this.getEntries = function() {
+            return this.opts.entries;
+        }
+    }
+
+    function CurrentSessionRenderer(session, selector) {
+        this.session = session;
+        this.selector = selector;
+
         this.render = function() {
-            arrLength = this.entries.length;
-            this.el.html("");
+            var entries = this.session.getEntries();
+            var arrLength = entries.length;
+            $(this.selector).html("");
 
             for (var i = 0; i < arrLength; i++) {
-                this._renderEntry(this.entries[i], i+1)
+                this._renderEntry(entries[i], i+1)
             };
         }
 
-        this._renderEntry = function(entry, number) {
-            var row = this._getTimeRow(number, entry.time.getFormatted());
-            var inforow = this._getTimeInfoRow(number, entry.scramble);
+        this.renderLastEntry = function() {
+            var entries = this.session.getEntries();
+            this._renderEntry(entries[entries.length - 1], entries.length);
+        }
 
-            inforow.prependTo(this.el)
-            row.prependTo(this.el)
+        this._renderEntry = function(entry, number) {
+            var row = this._getTimeRow(number, entry.time);
+            var inforow = this._getTimeInfoRow(number, entry.scramble);
+            var element = $(this.selector);
+
+            inforow.prependTo(element);
+            row.prependTo(element);
         }
 
         this._getTimeRow = function(num, time) {
@@ -207,15 +210,15 @@ $(function() {
     }
 
     // Timer class definition
-    function Timer(timerEl, historyEl, scrambleEl) {
-        this.history = new TimeHistory(historyEl);
-        this.cube = new Cube(scrambleEl);
-
+    function Timer(opts) {
         this.started = false;
         this.canRun = true;
         this.timeStart = false;
         this.timeCurrent = false;
-        this.el = timerEl;
+
+        this.opts = $.extend({
+            selector: "#time"
+        }, opts || {});
 
         this.start = function() {
             if (!this.started && this.canRun) {
@@ -251,8 +254,8 @@ $(function() {
 
             this.timeCurrent = new Date(timeDiff);
 
-            var entry = new SolveTime(this.timeCurrent);
-            this.el.html(entry.getFormatted());
+            var entry = new SolveTime({ time: this.timeCurrent });
+            $(this.opts.selector).trigger("onTimerTick", [entry]);
 
             if (this.started) {
                 var _this = this;
@@ -260,17 +263,39 @@ $(function() {
                     _this.runner();
                 }, 1)
             } else {
-                this.history.addEntry(entry, "", false);
+                $(this.opts.selector).trigger("onTimerStop", [entry, ""])
             }
         }
     }
 
     // Create Timer object and bind to events
-    timer = new Timer(
-        $('#time'),
-        $('#time-history'),
-        $("#scramble")
-    )
+    timer = new Timer({
+        selector: "#time"
+    });
+
+    // Create current solve session object
+    session = new SolveSession();
+    sessionRenderer = new CurrentSessionRenderer(session, "#time-history");
+    scrambler = new CubeScrambler();
+
+    $("#scramble").html(
+        scrambler.scramble().join(" ")
+    );
+
+    // Display time
+    $("#time").on("onTimerTick", function(event, entry) {
+        $(this).html(entry.toString());
+    });
+
+    // Add session entry when stopping timer
+    $("#time").on("onTimerStop", function(event, entry, scramble) {
+        session.addEntry(entry, $("#scramble").html());
+        sessionRenderer.renderLastEntry();
+
+        $("#scramble").html(
+            scrambler.scramble().join(" ")
+        )
+    });
     
     document.addEventListener('keydown', function(e) {
         if(e.keyCode == 32) {
@@ -285,9 +310,4 @@ $(function() {
             e.preventDefault();
         }
     }, false);
-
-    $("table#history-table tbody").on("click", "tr.history-time", function() {
-        alert("Alert!");
-    });
-
 });
