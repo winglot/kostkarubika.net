87655f54000749f415ae2cfe42cb9aaa5da279e5
Wojciech Inglot <wojciech@inglot.net.pl>
2015-09-02 22:30:14 +0200

    Cleanup templates

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,7 +1,4 @@
 $(function() {
-    // Init Material Design
-    $.material.init();
-
     // Style tables
     $("article table").addClass("table").addClass("table-responsive").addClass("table-striped");
     $("article table th:first-child").addClass("col-sm-3");
