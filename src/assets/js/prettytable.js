615cc067a4bd8b7cd2f2a250cb148ed78e48de94
Wojciech Inglot <wojciech@inglot.net.pl>
2016-09-24 20:12:13 +0200

    Replace Bootstrap Material Theme with MUICSS

--- src/assets/js/prettytable.js
+++ src/assets/js/prettytable.js
@@ -1,7 +1,13 @@
-$(function() {
-    table = $("article table");
-    table
-      .addClass("table")
-      .addClass("table-striped")
-      .wrap("<div class=\"table-responsive\"></div>")
-});
+document.addEventListener('DOMContentLoaded', function() {
+    tables = document.querySelectorAll("article table");
+    [].forEach.call(tables, function(table) {
+      table.classList.add("mui-table");
+      table.classList.add("mui-table--bordered");
+
+      var wrapper = document.createElement('div');
+      wrapper.classList.add("cube-table--responsive");
+
+      table.parentNode.insertBefore(wrapper, table);
+      wrapper.appendChild(table);
+    });
+}, false);
